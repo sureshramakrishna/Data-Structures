5a55a5bf60ca62b361a527cda9b2b50c05e7d390
Suresh-Ramakrishna <sureshramakrishna.backups@gmail.com>
2020-07-18 15:24:01 +0530

    Queue to throw exception instead of writing message on overflow and empty

--- Queues/Program.cs
+++ Queues/Program.cs
@@ -4,60 +4,65 @@ namespace Queues
 {
     class Queue<T>
     {
-        int front = 0, rear = -1, count = 0, maxSize;
-        T[] items;
+        int _front, _rear, _maxSize;
+        T[] _items;
+
+        //Can be full if front is in 0th position and rear is at the end or if front is somewhere in middle and Rear is just behind that.
+        public bool IsFull => (_front == _rear + 1) || (_front == 0 && _rear == _maxSize - 1);
+        public bool IsEmpty => _front == -1;
+
         public Queue(int capacity)
         {
-            maxSize = capacity;
-            items = new T[capacity];
+            _maxSize = capacity;
+            _front = _rear = -1;
+            _items = new T[capacity];
         }
+
         public void Enqueue(T item)
         {
-            if (count == maxSize)
-            {
-                Console.WriteLine("Queue Full...");
-                return;
-            }
-            Console.WriteLine($"Inserting {item}");
-            rear = (rear + 1) % maxSize;
-            items[rear] = item;
-            count++;
+            if (IsFull)
+                throw new Exception("Queue full!");
+            if (_front == -1)
+                _front = 0;
+            _rear = (_rear + 1) % _maxSize;
+            _items[_rear] = item;
         }
         public T Dequeue()
         {
-            if (count == 0)
-            {
-                Console.WriteLine("Queue empty...");
-                return default; //we are returning default here, you can choose to throw an error instead.
-            }
-            Console.WriteLine($"Removing {items[front]}");
-            var item = items[front];
-            front = (front + 1) % maxSize;
-            count--;
+            if (IsEmpty)
+                throw new Exception("Queue empty!");
+            var item = _items[_front];
+            if (_front == _rear)
+                _front = _rear = -1;
+            else
+                _front = (_front + 1) % _maxSize;
             return item;
         }
         public T Peek()
         {
-            if (count == 0)
-            {
-                Console.WriteLine("Queue empty...");
-                return default; //we are returning default here, you can choose to throw an error instead.
-            }
-            return items[front];
+            if (IsEmpty)
+                throw new Exception("Queue empty!");
+            return _items[_front];
         }
     }
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] _)
         {
             Queue<int> queue = new Queue<int>(5);
+
             for (int i = 0; i <= 5; i++) //Using <= to simulate overflow.
-                queue.Enqueue(i);
+                if (!queue.IsFull)
+                    queue.Enqueue(i);
+
             queue.Dequeue();
             queue.Enqueue(0);
+
             Console.WriteLine($"Peeking Queue: {queue.Peek()}");
+
             for (int i = 0; i <= 5; i++) //Using <= to simulate underflow.
-                queue.Dequeue();
+                if (!queue.IsEmpty)
+                    queue.Dequeue();
         }
     }
 }
