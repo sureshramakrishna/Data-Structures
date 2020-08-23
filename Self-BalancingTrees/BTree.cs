8836eb9a9ad9ec1ca7a33776eb69afebf26ddf31
Suresh Ramakrishna <sureshramakrishna.backups@gmail.com>
2020-08-23 15:34:26 +0530

    Adding comments

--- Self-BalancingTrees/BTree.cs
+++ Self-BalancingTrees/BTree.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace Self_BalancingTrees
 {
+    /// <summary>
+    /// Watch this video for deletion.
+    /// https://www.youtube.com/watch?v=GKa_t7fF8o0
+    /// </summary>
     public class Node
     {
         public int MAX, nCount;
@@ -21,6 +25,10 @@ namespace Self_BalancingTrees
             Children = new Node[MAX + 2];
             IsLeaf = true;
         }
+        /// <summary>
+        /// Makes space for the new key by pushing the existing keys to the right and inserts the new key at the appropriate position.
+        /// </summary>
+        /// <param name="k"></param>
         public void Push(int k)
         {
             int i;
@@ -30,17 +38,28 @@ namespace Self_BalancingTrees
                 if (!IsLeaf)
                     Children[i + 1] = Children[i];
             }
+            //needed when new key is being inserted in 1st index, then we should also push the first child to the right.
             if(i == 0 && !IsLeaf)
                 Children[1] = Children[0];
             Keys[i] = k;
             nCount++;
         }
-        public int GetChildIndex(int k)
+        /// <summary>
+        /// Gets the index of the subtree which might contain the key
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public int GetChildIndex(int key)
         {
             int index;
-            for (index = 0; index < nCount && Keys[index] < k; index++) ;
+            for (index = 0; index < nCount && Keys[index] < key; index++) ;
             return index;
         }
+        /// <summary>
+        /// returns the index of the key if exists, else returns int.MinValue.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         public int HasKey(int key)
         {
             for (int i = 0; i < nCount; i++)
@@ -48,6 +67,11 @@ namespace Self_BalancingTrees
                     return i;
             return int.MinValue;
         }
+        /// <summary>
+        /// Merges current node with node n. Copies all the keys and children to the current node.
+        /// NOTE: this method is invoked after adding parent key to the current node. See merge operation for more details.
+        /// </summary>
+        /// <param name="n"></param>
         public void Merge(Node n)
         {
             for (int index = nCount; index < n.nCount + this.nCount; index++)
@@ -80,6 +104,25 @@ namespace Self_BalancingTrees
                 root.Children[0] = left;
                 root.Children[1] = right;
                 root.IsLeaf = false;
+            }
+        }
+
+        public void Insert(Node root, int key)
+        {
+            if (root.IsLeaf)
+                root.Push(key);
+            else
+            {
+                var index = root.GetChildIndex(key);
+                Insert(root.Children[index], key);
+                if (root.Children[index].IsOverLoaded)
+                {
+                    var splitKey = Split(root.Children[index], out Node left, out Node right);
+                    root.Push(splitKey);
+                    root.Children[index] = left;
+                    root.Children[index + 1] = right;
+                    root.IsLeaf = false;
+                }
             }
         }
         /// <summary>
@@ -115,25 +158,6 @@ namespace Self_BalancingTrees
             return x.Keys[splitOrder];
         }
 
-        public void Insert(Node root, int key)
-        {
-            if (root.IsLeaf)
-                root.Push(key);
-            else
-            {
-                var index = root.GetChildIndex(key);
-                Insert(root.Children[index], key);
-                if (root.Children[index].IsOverLoaded)
-                {
-                    var splitKey = Split(root.Children[index], out Node left, out Node right);
-                    root.Push(splitKey);
-                    root.Children[index] = left;
-                    root.Children[index + 1] = right;
-                    root.IsLeaf = false;
-                }
-            }
-        }
-
         public void Remove(int key)
         {
             if (root.HasKey(key) != int.MinValue)
@@ -155,6 +179,12 @@ namespace Self_BalancingTrees
             if (root.nCount == 0)
                 root = root.Children[0];
         }
+        /// <summary>
+        /// Delete the key from the node and pulls the keys and children 1 step to the left.
+        /// Optional region is to setting the slots after nCount to 0 and null for children, this is for better readability purpose. 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="key"></param>
         void DeleteFromNode(Node x, int key)
         {
             int index;
@@ -167,12 +197,26 @@ namespace Self_BalancingTrees
                     x.Children[i] = x.Children[i + 1];
             }
             x.nCount--;
+
+            #region optional
             for (int i = x.nCount; i < x.MAX; i++)
                 x.Keys[i] = 0;
             for (int i = x.nCount + 1; i <= x.MAX; i++)
                 x.Children[i] = null;
+            #endregion optional
         }
 
+        /// <summary>
+        /// Borrows the key from its sibling and pushes the parent key down
+        /// if sibling is predecessor, get the last key, else get the first key
+        /// if sibling is predecessor, assign the right child of sibling as 1st child of current node else assign the first child of sibling as last child of current node.
+        /// 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="parent"></param>
+        /// <param name="pIndex"></param>
+        /// <param name="sIndex"></param>
+        /// <param name="isPredecessor"></param>
         void Borrow(Node x, Node parent, int pIndex, int sIndex, bool isPredecessor)
         {
             var sibling = parent.Children[sIndex];
@@ -185,6 +229,7 @@ namespace Self_BalancingTrees
                 if(isPredecessor)
                 { 
                     x.Children[0] = sibling.Children[sibling.nCount];
+                    //We are deleting directly here instead of using DeleteFromNode, because deleting last key needs special handling.
                     sibling.nCount--;
                     sibling.Keys[sibling.nCount] = 0;
                     sibling.Children[sibling.nCount + 1] = null;
@@ -199,6 +244,12 @@ namespace Self_BalancingTrees
                 DeleteFromNode(sibling, sItem);
         }
 
+        /// <summary>
+        /// Merges 2 nodes based on the parent.
+        /// </summary>
+        /// <param name="parent"></param>
+        /// <param name="cIndex"></param>
+        /// <param name="sIndex"></param>
         private void Merge(Node parent, int cIndex, int sIndex)
         {
             var x = parent.Children[cIndex];
@@ -220,7 +271,12 @@ namespace Self_BalancingTrees
                 parent.Children[0] = x;
             }
         }
-
+        /// <summary>
+        /// Decides whether borrow is possible, if not, does merge.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="parent"></param>
+        /// <param name="cIndex"></param>
         void BorrowOrMerge(Node x, Node parent, int cIndex)
         {
             var leftSibling = cIndex - 1;
